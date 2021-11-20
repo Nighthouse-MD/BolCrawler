4590fd9eb1e35db9ababcaea0970a90fa52e97ce
Maxime Deschamps <maxime.deschamps@involved-it.be>
2021-11-20 22:57:24 +0100

    update script

--- Resources/bolTrackerTask.bat
+++ Resources/bolTrackerTask.bat
@@ -1,2 +1,2 @@
-@REM D: && cd C:\code\bolcrawler\bolcrawler && git.exe reset --h && git.exe pull
+D: && cd C:\code\bolcrawler\bolcrawler && git.exe reset --h && git.exe pull
 "C:\Users\bolderadmin\AppData\Local\Programs\Python\Python38\python.exe" "C:\code\bolcrawler\bolcrawler\__main__.py"
