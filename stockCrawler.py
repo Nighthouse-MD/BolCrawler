72152fe763df22511c0777128a206d47dce588c9
Maxime Deschamps <maxime.deschamps@involved-it.be>
2021-11-22 16:08:36 +0100

    shuffle lift of records to track

--- stockCrawler.py
+++ stockCrawler.py
@@ -5,10 +5,12 @@ from Crawler.BolCrawler import handlerCrawlForOneProductAllSellers
 from Data.productToTrack import list_all
 from Data.db import create_connection
 from Constants import Constants
+import random
 
 
 def run():
     conn = create_connection(Constants.DB_PATH)
     productsToTrack = list_all(conn)
+    random.shuffle(productsToTrack)
     for i in range(len(productsToTrack)):
         handlerCrawlForOneProductAllSellers(productsToTrack[i])
