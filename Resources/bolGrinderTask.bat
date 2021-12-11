ff0bdf89e3107d712cf1c865ee2bdc07f0c7e3be
Maxime Deschamps <maxime.deschamps@involved-it.be>
2021-12-11 14:51:13 +0100

    separate grinder task

---
+++ Resources/bolGrinderTask.bat
@@ -0,0 +1,2 @@
+C: && cd C:\code\bolcrawler\bolcrawler && git.exe reset --h && git.exe pull
+"C:\Users\bolderadmin\AppData\Local\Programs\Python\Python38\python.exe" "C:\code\bolcrawler\bolcrawler\__main_grinder__.py"
